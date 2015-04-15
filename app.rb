7942eb05003f883be6472dfc7c3795cc917da6fb
Erik Doernenburg <erik@doernenburg.com>
2015-04-15 11:11:42 +0200

    Specific templates for different charts (again).

--- app.rb
+++ app.rb
@@ -11,8 +11,8 @@ get '/' do
   erb :index
 end
 
-get '/chart/:name' do
-  erb :chart, :locals => {:name => params[:name]}
+get '/chart/timeline' do
+  erb :timeline
 end
 
 get '/data/timeline.json' do
@@ -20,12 +20,25 @@ get '/data/timeline.json' do
   flatten_entries(churn_by_file_by_day()).to_json
 end
 
+get '/chart/histogram' do
+  erb :histogram
+end
+
 get '/data/histogram.json' do
   content_type :json
   churn_by_file().to_json
+end
+
+get '/chart/matrix' do
+  erb :matrix
 end
 
 get '/data/matrix.json' do
   content_type :json
   read_log2().to_json
 end
+
+get '/chart/:name' do
+  erb "no such chart #{params[:name]}", :locals => {:name => params[:name]}
+end
+
